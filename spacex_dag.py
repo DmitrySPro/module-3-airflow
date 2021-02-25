f06f8578d48883cb94e2c1cb817e5ccd38530bd7
DmitrySPro <68500456+DmitrySPro@users.noreply.github.com>
2021-02-25 19:35:37 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -25,13 +25,13 @@ for rct in ["all", "falcon1", "falcon9", "falconheavy"]:
         bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r {{ params.rocket }}", 
         params={"rocket": rct}
         dag=dag
-    )
+        )
 
     t2 = BashOperator(
         task_id="print_data", 
         bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
         params={"rocket": rct}, # falcon1/falcon9/falconheavy
         dag=dag
-    )
+        )
 
     t1 >> t2
