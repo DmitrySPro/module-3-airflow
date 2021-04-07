c07848ced6c7f547d6471ef57a027c785fc13134
DmitrySPro <68500456+DmitrySPro@users.noreply.github.com>
2021-04-07 16:48:54 +0300

    Update data_lake_etl.py

--- data_lake_etl.py
+++ data_lake_etl.py
@@ -23,10 +23,47 @@ ods_billing = DataProcHiveOperator(
     dag=dag,
     query="""
         insert overwrite table pdmitry.ods_billing partition (year='{{ execution_date.year }}') 
-        select * from pdmitry.stg_billing where year(created_at) = {{ execution_date.year }};
+        select user_id, billing_period, service, tariff, cast(sum as FLOAT), cast(created_at as DATE) from pdmitry.stg_billing where year(cast(created_at as DATE)) = {{ execution_date.year }};
     """,
     cluster_name='cluster-dataproc',
     job_name=USERNAME + '_ods_billing_{{ execution_date.year }}_{{ params.job_suffix }}',
     params={"job_suffix": randint(0, 100000)},
     region='europe-west3',
 )
+ods_traffic = DataProcHiveOperator(
+    task_id='ods_traffic',
+    dag=dag,
+    query="""
+        insert overwrite table pdmitry.ods_traffic partition (year='{{ execution_date.year }}') 
+        select user_id, cast(`timestamp` as TIMESTAMP), device_id, device_ip_addr, bytes_sent, bytes_received from pdmitry.stg_traffic where year(cast(`timestamp` as TIMESTAMP)) = {{ execution_date.year }};
+    """,
+    cluster_name='cluster-dataproc',
+    job_name=USERNAME + '_ods_traffic_{{ execution_date.year }}_{{ params.job_suffix }}',
+    params={"job_suffix": randint(0, 100000)},
+    region='europe-west3',
+)
+ods_issue = DataProcHiveOperator(
+    task_id='ods_issue',
+    dag=dag,
+    query="""
+        insert overwrite table pdmitry.ods_issue partition (year='{{ execution_date.year }}') 
+        select cast(user_id as INT), cast(start_time as TIMESTAMP), cast(end_time as TIMESTAMP), title, description, service from pdmitry.stg_issue where year(cast(start_time as TIMESTAMP)) = {{ execution_date.year }};
+    """,
+    cluster_name='cluster-dataproc',
+    job_name=USERNAME + '_ods_issue_{{ execution_date.year }}_{{ params.job_suffix }}',
+    params={"job_suffix": randint(0, 100000)},
+    region='europe-west3',
+)
+ods_payment = DataProcHiveOperator(
+    task_id='ods_payment',
+    dag=dag,
+    query="""
+        insert overwrite table pdmitry.ods_payment partition (year='{{ execution_date.year }}') 
+        select user_id, pay_doc_type, cast(pay_doc_num as INT), account, phone, billing_period, cast(pay_date as DATE), cast(sum as FLOAT) from pdmitry.stg_payment where year(cast(pay_date as DATE)) = {{ execution_date.year }};
+    """,
+    cluster_name='cluster-dataproc',
+    job_name=USERNAME + '_ods_payment_{{ execution_date.year }}_{{ params.job_suffix }}',
+    params={"job_suffix": randint(0, 100000)},
+    region='europe-west3',
+)
+
