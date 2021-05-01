72a5a2b7de1f24294a2c151808d48a9cc9ceb6d9
DmitrySPro <68500456+DmitrySPro@users.noreply.github.com>
2021-05-01 20:48:09 +0300

    Update pdmitry.dwh_etl.py

--- pdmitry.dwh_etl.py
+++ pdmitry.dwh_etl.py
@@ -90,29 +90,34 @@ dds_hub_account = PostgresOperator(
     """
 )
 
+dds_hub_payment = PostgresOperator(
+    task_id="dds_hub_payment",
+    dag=dag,
+    sql="""
+        insert into pdmitry.dds_hub_payment (PAYMENT_PK, PAY_DOC_TYPE_KEY, PAY_DOC_NUM_KEY, LOAD_DATE, RECORD_SOURCE)
+        SELECT PAYMENT_PK, PAY_DOC_TYPE_KEY, PAY_DOC_NUM_KEY, LOAD_DATE, RECORD_SOURCE
+        FROM pdmitry.view_hub_payment_etl
+
+    """
+)
+
+
+
 all_hubs_loaded = DummyOperator(task_id="all_hubs_loaded", dag=dag)
 
 
-ods_loaded >> dds_hub_user >> dds_hub_billing_period >> dds_hub_account >> all_hubs_loaded
+ods_loaded >> dds_hub_user >> dds_hub_billing_period >> dds_hub_account >> dds_hub_payment >> all_hubs_loaded
 
-dds_link_user_account = PostgresOperator(
-    task_id="dds_link_user_account",
+dds_link_user_account_billing_payment = PostgresOperator(
+    task_id="dds_link_user_account_billing_payment",
     dag=dag,
     sql="""
-    insert into pdmitry.dds_link_user_account (USER_ACCOUNT_PK, USER_PK, ACCOUNT_PK, LOAD_DATE, RECORD_SOURCE)
-    SELECT USER_ACCOUNT_PK, USER_PK, ACCOUNT_PK, LOAD_DATE, RECORD_SOURCE
-    FROM pdmitry.view_link_user_account_etl
+    insert into pdmitry.dds_link_user_account_billing_payment (PAY_PK, USER_PK, ACCOUNT_PK, PAYMENT_PK, BILLING_PERIOD_PK, LOAD_DATE, RECORD_SOURCE)
+    SELECT PAY_PK, USER_PK, ACCOUNT_PK, PAYMENT_PK, BILLING_PERIOD_PK, LOAD_DATE, RECORD_SOURCE
+    FROM pdmitry.view_link_user_account_billing_payment_etl
     """
 )
-dds_link_account_billing_payment = PostgresOperator(
-    task_id="dds_link_account_billing_payment",
-    dag=dag,
-    sql="""
-    insert into pdmitry.dds_link_account_billing_payment (PAY_PK, ACCOUNT_PK, PAYMENT_PK, BILLING_PERIOD_PK, LOAD_DATE, RECORD_SOURCE)
-    SELECT PAY_PK, ACCOUNT_PK, PAYMENT_PK, BILLING_PERIOD_PK, LOAD_DATE, RECORD_SOURCE
-    FROM pdmitry.view_link_account_billing_payment_etl
-    """
-)
+
 
 all_links_loaded = DummyOperator(task_id="all_links_loaded", dag=dag)
 
@@ -183,5 +188,35 @@ dds_sat_payment = PostgresOperator(
      select * from records_to_insert
     """
 )   
-
-all_links_loaded >> dds_sat_user >> dds_sat_payment
+dds_sat_mdm = PostgresOperator(
+    task_id="dds_sat_mdm",
+    dag=dag,
+    sql="""
+    insert into pdmitry.dds_sat_mdm (USER_PK, legal_type, district, registered_at, billing_mode, is_vip, mdm_hashdif, LOAD_DATE, RECORD_SOURCE)
+    with source_data as (
+    select a.USER_PK, a.legal_type, a.district, a.registered_at, a.billing_mode, a.is_vip, a.mdm_hashdif, a.LOAD_DATE, a.RECORD_SOURCE from pdmitry.ods_mdm_hashed as a
+    WHERE a.LOAD_DATE = '{{ execution_date }}'::TIMESTAMP
+    ),
+     update_records as (
+        select a.USER_PK, a.legal_type, a.district, a.registered_at, a.billing_mode, a.is_vip, a.mdm_hashdif, a.LOAD_DATE, a.RECORD_SOURCE from pdmitry.dds_sat_mdm as a
+        join source_data as b on a.USER_PK = b.USER_PK AND a.LOAD_DATE <= (SELECT max(LOAD_DATE) from source_data)
+     ),
+     latest_records as (
+         select * from (
+                       select c.USER_PK, c.MDM_HASHDIF, c.LOAD_DATE,
+                       CASE WHEN RANK() over (partition by c.USER_PK order by c.LOAD_DATE DESC) = 1
+                       THEN 'Y' ELSE 'N' END AS latest
+                FROM update_records as c
+            ) as s
+            WHERE latest = 'Y'
+            ),
+     records_to_insert as (
+         select distinct e.USER_PK, e.legal_type, e.district, e.registered_at, e.billing_mode, e.is_vip, e.mdm_hashdif, e.LOAD_DATE, e.RECORD_SOURCE
+         from source_data as e
+         left join latest_records on latest_records.MDM_HASHDIF=e.MDM_HASHDIF and latest_records.USER_PK=e.USER_PK
+         where latest_records.MDM_HASHDIF is null
+          )
+     select * from records_to_insert
+    """
+)
+all_links_loaded >> dds_sat_user >> dds_sat_payment >> dds_sat_mdm
