b78a3e5642213c3b67674961f9e32d6013c3a167
DmitrySPro <68500456+DmitrySPro@users.noreply.github.com>
2021-05-03 14:54:02 +0300

    Update pdmitry_dwh_etl_v2.py

--- pdmitry_dwh_etl_v2.py
+++ pdmitry_dwh_etl_v2.py
@@ -219,11 +219,11 @@ dds_sat_mdm_v2 = PostgresOperator(
     sql="""
     insert into pdmitry.dds_sat_mdm_v2 (USER_PK, legal_type, district, registered_at, billing_mode, is_vip, mdm_hashdif, LOAD_DATE, RECORD_SOURCE)
     with source_data as (
-    select a.USER_PK, a.legal_type, a.district, a.registered_at, a.billing_mode, a.is_vip, a.mdm_hashdif, a.LOAD_DATE, a.RECORD_SOURCE from pdmitry.ods_mdm_hashed_v2 as a
+    select a.USER_PK, a.legal_type, a.district, a.registered_at, a.billing_mode, a.is_vip, a.mdm_hashdif, a.effective_from, a.LOAD_DATE, a.RECORD_SOURCE from pdmitry.ods_mdm_hashed_v2 as a
     WHERE a.LOAD_DATE = '{{ execution_date }}'::TIMESTAMP
     ),
      update_records as (
-        select a.USER_PK, a.legal_type, a.district, a.registered_at, a.billing_mode, a.is_vip, a.mdm_hashdif, a.LOAD_DATE, a.RECORD_SOURCE from pdmitry.dds_sat_mdm_v2 as a
+        select a.USER_PK, a.legal_type, a.district, a.registered_at, a.billing_mode, a.is_vip, a.mdm_hashdif, a.effective_from, a.LOAD_DATE, a.RECORD_SOURCE from pdmitry.dds_sat_mdm_v2 as a
         join source_data as b on a.USER_PK = b.USER_PK AND a.LOAD_DATE <= (SELECT max(LOAD_DATE) from source_data)
      ),
      latest_records as (
@@ -236,7 +236,7 @@ dds_sat_mdm_v2 = PostgresOperator(
             WHERE latest = 'Y'
             ),
      records_to_insert as (
-         select distinct e.USER_PK, e.legal_type, e.district, e.registered_at, e.billing_mode, e.is_vip, e.mdm_hashdif, e.LOAD_DATE, e.RECORD_SOURCE
+         select distinct e.USER_PK, e.legal_type, e.district, e.registered_at, e.billing_mode, e.is_vip, e.mdm_hashdif, e.effective_from, e.LOAD_DATE, e.RECORD_SOURCE
          from source_data as e
          left join latest_records on latest_records.MDM_HASHDIF=e.MDM_HASHDIF and latest_records.USER_PK=e.USER_PK
          where latest_records.MDM_HASHDIF is null
